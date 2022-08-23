20c8c7b56f258c8076ab6fcc52a61f4ebd5c73a1
DennsHartstock <dennis.hartstock@web.de>
2022-08-23 08:19:27 +0200

    add JobIntentService

--- app/src/main/java/ru/sumin/servicestest/MyJobIntentService.kt
+++ app/src/main/java/ru/sumin/servicestest/MyJobIntentService.kt
@@ -1,19 +1,18 @@
 package ru.sumin.servicestest
 
-import android.app.IntentService
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
+import androidx.core.app.JobIntentService
 
-class MyIntentService2 : IntentService(NAME) {
+class MyJobIntentService : JobIntentService() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate() {
         super.onCreate()
         log("onCreate")
-        setIntentRedelivery(true)
     }
 
     override fun onDestroy() {
@@ -21,27 +20,35 @@ class MyIntentService2 : IntentService(NAME) {
         log("onDestroy")
     }
 
-    override fun onHandleIntent(intent: Intent?) {
+    override fun onHandleWork(intent: Intent) {
         log("onHandleIntent")
-        val page = intent?.getIntExtra(PAGE, 0) ?: 0
+        val page = intent.getIntExtra(PAGE, 0)
         for (i in 0 until 5) {
             Thread.sleep(1000)
             log("Timer $i $page")
         }
     }
 
-
     private fun log(message: String) {
-        Log.d("SERVICE_TAG", "MyIntentService2: $message")
+        Log.d("SERVICE_TAG", "MyJobIntentService: $message")
     }
 
     companion object {
 
         private const val PAGE = "page"
-        private const val NAME = "MyIntentService"
+        private const val JOB_ID = 111
 
-        fun newIntent(context: Context, page: Int): Intent {
-            return Intent(context, MyIntentService2::class.java).apply {
+        fun enqueue(context: Context, page: Int) {
+            enqueueWork(
+                context,
+                MyJobIntentService::class.java,
+                JOB_ID,
+                newIntent(context, page)
+            )
+        }
+
+        private fun newIntent(context: Context, page: Int): Intent {
+            return Intent(context, MyJobIntentService::class.java).apply {
                 putExtra(PAGE, page)
             }
         }
